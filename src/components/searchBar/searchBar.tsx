66af94032c2b0ea67c36fc60db4d0d951e6a12a5
franciscoarocas <54645175+franciscoarocas@users.noreply.github.com>
2025-06-21 14:47:06 +0200

    Changed search bar

--- src/components/searchBar/searchBar.tsx
+++ src/components/searchBar/searchBar.tsx
@@ -1,20 +1,22 @@
 
 import { Form, InputGroup } from 'react-bootstrap'
 
+import { FaSearch, FaRegTimesCircle  } from "react-icons/fa";
+
 
 const SearchBar = ({ value, onChange }) => {
 
   return (
     <InputGroup className="mb-3">
-      <InputGroup.Text>🔍</InputGroup.Text>
+      <InputGroup.Text><FaSearch /></InputGroup.Text>
       <Form.Control
         type="search"
         value={value}
         onChange={e => onChange(e.target.value)}
       />
       {value && (
-        <InputGroup.Text role="button" onClick={() => onChange('')}>
-          ✖
+        <InputGroup.Text onClick={() => onChange('')} style={{ cursor: 'pointer' }}>
+          <FaRegTimesCircle />
         </InputGroup.Text>
       )}
     </InputGroup>
